f3515a21c232bb66bedbf7d7b23cd1ce4019b79d
Ian Oberst <areson@gmail.com>
2012-04-24 18:39:42 -0700

    Made the snapshots list observable.

--- knockout.snapshot.js
+++ knockout.snapshot.js
@@ -1,31 +1,47 @@
 ko.extenders.snapshot = function(target, snapshotFunction) {
-    var _snapshots = [];
-
+    var _snapshots = ko.observableArray([]);                
+    
+    //Setup a dependent observable to track
+    //the list of snapshots
+    target.snapshots = ko.dependentObservable({
+        read: function() {
+            return _snapshots();
+        },
+        write: function() {}
+    });
+    
     if(!$.isFunction(snapshotFunction))
         snapshotFunction = null;
 
     //This is the function that controls how the 
     //snapshots get mapped back to the observable.
     //The data that is passed is JSON.
-    var _snapshotFunction = snapshotFunction || function(data, target) {
-        target($.parseJSON(data));
+    var _snapshotFunction = snapshotFunction || function(snapshot, target) {
+        target($.parseJSON(snapshot.data));
     }
 
-    target.snapshot = function() {
-        _snapshots.push(ko.toJSON(target));
+    target.snapshot = function(name) {
+        var snapshotName = name || "Snapshot";
+        _snapshots.push({
+            index: _snapshots().length,
+            name: snapshotName,
+            data: ko.toJSON(target)
+        });
     }
-
-    //Overrired the 
 
     target.revert = function(index) {
-        if (index === undefined && _snapshots.length > 0) 
+        var snapshotLength = _snapshots().length;
+
+        if (index === undefined && snapshotLength > 0) 
             _snapshotFunction(_snapshots.pop(), target);
-        else if (index < _snapshots.length) 
-            _snapshotFunction(_snapshots.splice(index, _snapshots.length - index)[0], target);
+        else if (index < snapshotLength) 
+            _snapshotFunction(_snapshots.splice(index, snapshotLength - index)[0], target);
+        else if (index.index != null && index.index < snapshotLength)  
+            _snapshotFunction(_snapshots.splice(index.index, snapshotLength - index.index)[0], target);
     }
 
     target.clearSnapshots = function() {
-        _snapshots = [];
+        _snapshots([]);
     }
 
     return (target);
